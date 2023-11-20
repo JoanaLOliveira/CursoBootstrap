39c90dc94b44efdd2130934d7134b995c9f88794
JoanaLOliveira <joanalmarcolino@gmail.com>
2023-11-20 15:41:07 -0300

    filtro com bootstrap

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function () {
 
     /* PROGRESS BAR */
@@ -111,7 +110,6 @@ $(document).ready(function () {
 
     });
 
-
     /* parallax */
     setTimeout(function () {
 
@@ -119,5 +117,43 @@ $(document).ready(function () {
         $('#apply-area').parallax({ imageSrc: 'img/pattern.png' });
 
     }, 250);
+
+    /* FILTRO DO PORTFÓLIO */
+    
+    $('.filter-btn').on('click', function(){
+    
+        let type = $(this).attr('id');
+        let boxes = $('.project-box');
+
+        $('.main-btn').removeClass('active');
+        $(this).addClass('active');
+
+        if (type == 'dsg-btn') {
+            eachBoxes('dsg', boxes)
+        } else if(type == 'dev-btn'){
+            eachBoxes('dev',boxes);
+        } else if(type == 'seo-btn'){
+            eachBoxes('seo',boxes);
+        } else {
+            eachBoxes('all',boxes);
+        }
+
+    });
+
+    function eachBoxes(type,boxes) {
+        
+        if (type == 'all') {
+            $(boxes).fadeIn();
+        } else {
+            $(boxes).each(function(){
+                if (!$(this).hasClass(type)) {
+                    $(this).fadeOut('slow');
+                } else {
+                    $(this).fadeIn();
+                }
+            });
+        }
+
+    }
 
 });
