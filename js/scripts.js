cef80ede96473f87e1ef47edd01238202345e7dc
JoanaLOliveira <joanalmarcolino@gmail.com>
2023-11-20 13:59:57 -0300

    Grid com parallax

--- js/scripts.js
+++ js/scripts.js
@@ -111,10 +111,12 @@ $(document).ready(function () {
 
     });
 
+
     /* parallax */
     setTimeout(function () {
 
         $('#data-area').parallax({ imageSrc: 'img/cidadeparallax.png' });
+        $('#apply-area').parallax({ imageSrc: 'img/pattern.png' });
 
     }, 250);
 
